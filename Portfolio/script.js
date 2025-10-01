3c26152402c5c6f00d1baa1692f3dbe469f95e04
balletFrancois18 <144659597+balletFrancois18@users.noreply.github.com>
2025-10-01 10:49:05 +0000

    avancement page interetV2

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,23 +1,11 @@
-// === SLIDER AU SURVOL ===
-document.querySelectorAll(".card").forEach(card => {
-  const images = card.querySelectorAll(".slider img");
+// SLIDER AUTO
+document.querySelectorAll(".slider").forEach(slider => {
+  const track = slider.querySelector(".slider-track");
+  const slides = slider.querySelectorAll("img");
   let index = 0;
-  let interval;
 
-  card.addEventListener("mouseenter", () => {
-    if (images.length > 1) {
-      interval = setInterval(() => {
-        images[index].classList.remove("active");
-        index = (index + 1) % images.length;
-        images[index].classList.add("active");
-      }, 3000); // toutes les 3 secondes
-    }
-  });
-
-  card.addEventListener("mouseleave", () => {
-    clearInterval(interval);
-    images.forEach(img => img.classList.remove("active"));
-    if (images.length > 0) images[0].classList.add("active");
-    index = 0;
-  });
+  setInterval(() => {
+    index = (index + 1) % slides.length;
+    track.style.transform = `translateX(-${index * 100}%)`;
+  }, 3000); // toutes les 3 sec
 });
